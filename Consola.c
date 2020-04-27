30a0f5c67b12ff8f7cc3067774750f9cf6d668bd
SamuelHornsbyAltares <47475011+SamuelHornsbyAltares@users.noreply.github.com>
2020-04-27 19:13:57 +0200

    Incluir juego tres en raya (tanto 1 Vs. 1 como Vs. CPU)

--- Consola.c
+++ Consola.c
@@ -8,6 +8,18 @@
 void inicio(void);
 void titulo(void);
 
+//PROTOTIPOS DE FUNCIONES TIC TAC TOE
+void tttcpu (char c [3][3]);
+void tttmulti (char c [3][3]);
+void Intro_Primera (char c [3][3]);
+void Tablero (char c [3][3]);
+void Intro_Valores (char c [3][3]);
+void Intro_IA (char c [3][3]);
+void Intro_User1 (char c [3][3]);
+void Intro_User2 (char c [3][3]);
+int Ganador1 (char c [3][3]);
+int Ganador2 (char c[3][3]);
+
 //PROTOTIPOS DE FUNCIONES ADIVINAR EL NÚMERO
 void adivinar(void);
 
@@ -47,6 +59,7 @@ int main()
 					printf("TIC TAC TOE 1 Vs.1\n\n");
 					printf("Jugador 1--->X\n\n");
 					printf("Jugador 2--->O\n\n\n\n");
+					tttmulti(c);
 					break;
 				case 2:
 					titulo();
@@ -54,6 +67,7 @@ int main()
 					printf("TIC TAC TOE Vs.CPU\n\n");
 					printf("Jugador 1--->X\n\n");
 					printf("CPU--->O\n\n\n\n");
+					tttcpu(c);
 					break;
 			}break;
 		case 2: //PIEDRA PAPEL TIJERA
@@ -122,6 +136,541 @@ void titulo(void)
 	printf("\t\t\t\t\t******CHINTENDO ZII LTD.******\t\t\t\n");
 	printf("\t\t\t\t\t      All rights reserved\t\t\t\n");
 	printf("\t\t\t\t\t    By SamuSung Electronics\t\t\t\n\n\n\n");
+}
+
+//FUNCIONES TIC TAC TOE
+void tttcpu (char c [3][3]){
+	int i,j;
+	i=0;
+	Intro_Primera(c);
+	do{
+		system("cls");
+		Tablero(c);
+		if(i % 2==0){
+			Intro_Valores(c);
+		}
+		else{
+			Intro_IA(c);
+		}
+		j=Ganador1(c);
+		i++;
+	}while(i<=9&&j==2);
+	system("cls");
+	Tablero(c);
+	if(j==0){
+		printf("VICTORIA, HAS GANADO\n\n");
+	}
+	else if(j==1){
+		printf("DERROTA, HAS PERDIDO\n\n");
+	}
+	else{
+		printf("EMPATE\n\n");
+	}
+}
+void tttmulti (char c [3][3]){
+	int i,j;
+	i=0;
+	Intro_Primera(c);
+	do{
+		system("cls");
+		Tablero(c);
+		if(i % 2==0){
+			Intro_User1(c);
+		}
+		else{
+			Intro_User2(c);
+		}
+		j=Ganador1(c);
+		i++;
+	}while(i<=9&&j==2);
+	system("cls");
+	Tablero(c);
+	if(j==0){
+		printf("VICTORIA, HAS GANADO\n\n");
+	}
+	else if(j==1){
+		printf("DERROTA, HAS PERDIDO\n\n");
+	}
+	else{
+		printf("EMPATE\n\n");
+	}
+}
+void Intro_Primera (char c [3][3]){
+	int i, j;
+	char aux;
+	aux='1';
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+		c [i][j]=aux++;	
+		}
+	}
+}
+void Intro_Valores (char c [3][3]){
+	char aux;
+	int i,j,k;
+	do{
+		do{
+			printf("Coloque una ficha: ");
+			fflush(stdin);
+			scanf("%c",&aux);
+		}while(aux<'1'||aux>'9');
+		k=0;
+		switch(aux){
+			case '1':{
+				i=0;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '2':{
+				i=0;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '3':{
+				i=0;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '4':{
+				i=1;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '5':{
+				i=1;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '6':{
+				i=1;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '7':{
+				i=2;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '8':{
+				i=2;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '9':{
+				i=2;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			
+		}
+	}while(k==1);
+	c [i][j]='X';
+}
+void Intro_IA (char c [3][3]){
+	int i,j,k;
+	srand(time(NULL));
+	do{
+		i=rand() % 3;
+		j=rand() % 3;
+		k=0;
+		if (c[i][j]=='X'||c[i][j]=='O'){
+			k=1;
+		}
+	}while(k==1);
+	c[i][j]='O';
+}
+void Tablero (char c [3][3]){
+	int i,j;
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			if(j<2){
+				printf(" %c |",c [i][j]);
+			}
+			else{
+				printf(" %c |",c [i][j]);
+			}	
+		}
+		if(i<2){
+			printf("\n-----------\n");
+		}
+	}
+	printf("\n\n");
+}
+int Ganador1 (char c[3][3]){
+	if(c[0][0]=='X'||c[0][0]=='O'){
+		if(c[0][0]==c[0][1]&&c[0][0]==c[0][2]){
+			if(c[0][0]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[0][0]==c[1][0]&&c[0][0]==c[2][0]){
+				if(c[0][0]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+	}
+	if(c[1][1]=='X'||c[1][1]=='O'){
+		if(c[1][1]==c[0][0]&&c[1][1]==c[2][2]){
+			if(c[1][1]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[1][1]==c[1][0]&&c[1][1]==c[1][2]){
+			if(c[1][1]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[1][1]==c[0][1]&&c[1][1]==c[2][1]){
+			if(c[1][1]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[1][1]==c[0][1]&&c[1][1]==c[2][0]){
+			if(c[1][1]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+	}
+	if(c[2][2]=='X'||c[2][2]=='O'){
+		if(c[2][2]==c[1][2]&&c[2][2]==c[0][2]){
+			if(c[2][2]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[2][2]==c[2][1]&&c[2][2]==c[2][0]){
+				if(c[2][2]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+	}
+	return 2;
+}
+int Ganador2 (char c[3][3]){
+	if(c[0][0]=='X'||c[0][0]=='O'){
+		if(c[0][0]==c[0][1]&&c[0][0]==c[0][2]){
+			if(c[0][0]='X'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[0][0]==c[1][0]&&c[0][0]==c[2][0]){
+				if(c[0][0]='O'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+	}
+	if(c[1][1]=='X'||c[1][1]=='O'){
+		if(c[1][1]==c[0][0]&&c[1][1]==c[2][2]){
+			if(c[1][1]='O'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[1][1]==c[1][0]&&c[1][1]==c[1][2]){
+			if(c[1][1]='O'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[1][1]==c[0][1]&&c[1][1]==c[2][1]){
+			if(c[1][1]='O'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[1][1]==c[0][1]&&c[1][1]==c[2][0]){
+			if(c[1][1]='O'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+	}
+	if(c[2][2]=='X'||c[2][2]=='O'){
+		if(c[2][2]==c[1][2]&&c[2][2]==c[0][2]){
+			if(c[2][2]='O'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+		if(c[2][2]==c[2][1]&&c[2][2]==c[2][0]){
+				if(c[2][2]='O'){
+				return 0; //He ganado
+			}
+			else{
+				return 1; //He perdido
+			}
+		}
+	}
+	return 2;
+}
+void Intro_User1 (char c [3][3]){
+	char aux;
+	int i,j,k;
+	do{
+		do{
+			printf("Coloque una ficha: ");
+			fflush(stdin);
+			scanf("%c",&aux);
+		}while(aux<'1'||aux>'9');
+		k=0;
+		switch(aux){
+			case '1':{
+				i=0;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '2':{
+				i=0;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '3':{
+				i=0;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '4':{
+				i=1;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '5':{
+				i=1;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '6':{
+				i=1;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '7':{
+				i=2;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '8':{
+				i=2;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '9':{
+				i=2;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			
+		}
+	}while(k==1);
+	c [i][j]='X';
+}
+void Intro_User2 (char c [3][3]){
+	char aux;
+	int i,j,k;
+	do{
+		do{
+			printf("Coloque una ficha: ");
+			fflush(stdin);
+			scanf("%c",&aux);
+		}while(aux<'1'||aux>'9');
+		k=0;
+		switch(aux){
+			case '1':{
+				i=0;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '2':{
+				i=0;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '3':{
+				i=0;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '4':{
+				i=1;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '5':{
+				i=1;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '6':{
+				i=1;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '7':{
+				i=2;
+				j=0;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '8':{
+				i=2;
+				j=1;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			case '9':{
+				i=2;
+				j=2;
+				if(c [i][j]=='X'||c [i][j]=='O'){
+					k=1;
+					printf("Casilla ocupada. Por favor, escoja otra: \n\n");
+				}
+				break;
+			}
+			
+		}
+	}while(k==1);
+	c [i][j]='O';
 }
 
 //FUNCIONES ADIVINAR EL NÚMERO
